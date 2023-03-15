0220c04824e57013d6dbfa7532460ec76df56330
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2023-03-15 13:22:45 +0900

    Time: 1 ms (83.31%), Space: 42.4 MB (15.55%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -48,3 +48,21 @@ class Solution {
         return true;
     }
 }
+
+/*
+queue - add() vs offer()
+- add : null 값을 add 하려고 시도하면 예외 뜸
+- offer : null 값을 offer하려고 하면 queue에 null이 들어가며 false를 리턴함
+
+Queue는 null을 offer할 수 있음
+Deque는 offer 써도 예외 뜸
+==============================
+
+queue - add() vs offer()
+- add : add() returns a nullPoint exception when we try to add null into the queue
+- offer : offer returns a false (bool) when we try to offer(add something into queue) null into the queue
+
+
+Queue can have null by offering null into the queue
+Deque cannot have null even by offering null into the queue
+*/
