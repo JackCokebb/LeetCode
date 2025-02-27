91885f24d7c57dcf15c0385f4d959220b50d8b22
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-02-27 23:29:43 +0900

    Time: 3 ms (98.27%), Space: 42.6 MB (60.55%) - LeetHub

--- 0481-magical-string/0481-magical-string.java
+++ 0481-magical-string/0481-magical-string.java
@@ -1,36 +1,22 @@
 class Solution {
-    String[] list;
-    int currIdx = 2;
-    int appendingIdx = 3;
-    int count = 1;
-
     public int magicalString(int n) {
-
-        if(n < 1) return 0;
-        if(n < 4) return 1;
-
-        list = new String[n + 1];
-        list[0] = "1";
-        list[1] = "2";
-        list[2] = "2";
-
-        int num = 1;
-
-        while(appendingIdx < n) {
-
-            for(int i = 0 ; i < Integer.parseInt(list[currIdx]) ; i++) {
-                list[appendingIdx] = String.valueOf(num);
-
-                if(num == 1 && appendingIdx < n) {
-                    count++;
-                }
+        if (n <= 0) return 0;
+        if (n <= 3) return 1;
+        
+        int[] a = new int[n + 1];
+        a[0] = 1; a[1] = 2; a[2] = 2;
+        int currIdx = 2, appendingIdx = 3, num = 1, count = 1;
+        
+        while (appendingIdx < n) {
+            for (int i = 0; i < a[currIdx]; i++) {
+                a[appendingIdx] = num;
+                if (num == 1 && appendingIdx < n) count++;
                 appendingIdx++;
             }
-
             num = num ^ 3;
             currIdx++;
         }
-
+        
         return count;
     }
 }
