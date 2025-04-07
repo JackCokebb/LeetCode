4cb7576d6b6e639c7ce6a6e503e12a1094309859
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-04-08 02:33:13 +0900

    Time: 5 ms (52.74%), Space: 44 MB (20.97%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,48 +1,27 @@
 class Solution {
     public String simplifyPath(String path) {
-        path = path.replaceAll("/+", "/");
-        String[] stripPath = path.split("/");
+        String[] components = path.split("/");
+        Stack<String> st = new Stack<>();
+
+        for (String comp : components) {
+            if (comp.equals("") || comp.equals(".")) {
+                continue;
+            }
+
+            if (comp.equals("..")) {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            } else {
+                st.push(comp);
+            }
+        }
+
         StringBuilder sb = new StringBuilder();
-
-        for (int i = stripPath.length - 1 ; i >= 0 ; i--) {
-            if (".".equals(stripPath[i])) {
-                stripPath[i] = "";
-            }
+        while (!st.isEmpty()) {
+            sb.insert(0, "/" + st.pop());
         }
 
-        for (int i = stripPath.length - 1 ; i >= 0 ; i--) {
-            if ("..".equals(stripPath[i]) && i > 0) {
-                int dotCount = 1;
-                stripPath[i] = "";
-                while (i >= 0 && "..".equals(stripPath[i - 1])) {
-                    stripPath[--i] = "";
-                    dotCount++;
-                }
-                --i;
-
-                while (dotCount-- > 0) {
-                    if (i >= 0 && stripPath[i] == "") {
-                        dotCount++;
-                        i--;
-                    } else if (i >= 0 && "..".equals(stripPath[i])) {
-                        dotCount += 2;
-                        stripPath[i] = "";
-                        i--;
-                    } else {
-                        stripPath[Math.max(0, i--)] = "";
-                    }
-                }
-                i++;
-            }
-        }
-
-        for (int i = 0 ; i < stripPath.length ; i++) {
-            if (stripPath[i] != null && "" != stripPath[i]) {
-                sb.append("/");
-                sb.append(stripPath[i]);
-            }
-        }
-
-        return sb.length() > 0 ? sb.toString() : "/";
+        return sb.length() == 0 ? "/" : sb.toString();        
     }
 }
