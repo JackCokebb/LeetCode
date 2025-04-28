8d9beb28c2e49b65928318f3f7eda09667ecdb05
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-04-28 23:00:14 +0900

    Time: 20 ms (1.75%), Space: 44.7 MB (7.61%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -0,0 +1,84 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        if(head == null) return new ListNode[k];
+
+        int count = 0;
+        ListNode curr = head;
+
+        while(curr != null) {
+            count++; 
+            curr = curr.next;
+        }
+        System.out.println("count = " + count);
+
+        int sectionCount = count / k;
+        int remainder = count % k;
+        
+        ListNode[] result = new ListNode[k];
+
+        curr = head;
+        
+        int currIdx = 0;
+        result[currIdx] = curr;
+
+        if(count < k) {
+            for(int i = 0 ; i < count ; i++) {
+                ListNode temp = curr.next;
+                curr.next = null;
+                curr = temp;
+                currIdx++;
+
+                if(currIdx < result.length) {
+                    result[currIdx] = curr;
+                }
+            }
+            return result;
+        }
+        
+
+        for(int i = 0 ; i < count ; i++) {
+            if(curr == null) break;
+
+            if((i + 1) % sectionCount == 0 && remainder > 0) {
+                System.out.println("(i + 1) % k == 0 && remainder > 0");
+                curr = curr.next;
+                ListNode temp = curr.next;
+                curr.next = null;
+                curr = temp;
+                remainder--;
+                currIdx++;
+
+                if(currIdx < result.length) {
+                    result[currIdx] = curr;
+                }
+
+            } else if((i + 1) % sectionCount == 0 && remainder <= 0) {
+                System.out.println("count % k == 0 && remainder <= 0");
+                ListNode temp = curr.next;
+                curr.next = null;
+                curr = temp;
+                currIdx++;
+
+                if(currIdx < result.length) {
+                    result[currIdx] = curr;
+                }
+
+            } else {
+                System.out.println("else");
+                curr = curr.next;
+            }
+        }
+
+        return result;
+    }
+}
