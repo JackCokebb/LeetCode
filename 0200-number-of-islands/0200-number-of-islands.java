5c4a651e94cbf0d06f4a5124ebe3029977708f12
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-06-24 00:48:47 +0900

    Time: 3 ms (84.62%), Space: 49.2 MB (84.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,52 +1,31 @@
-class Solution {
+public class Solution {
+
+    private int n;
+    private int m;
+
     public int numIslands(char[][] grid) {
-        int result = 0;
-        Deque<int[]> queue = new ArrayDeque<>();
-
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-
-        for (int i = 0 ; i < grid.length ; i++) {
-            for (int j = 0 ; j < grid[0].length ; j++) {
+        int count = 0;
+        n = grid.length;
+        if (n == 0)
+            return 0;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
                 if (grid[i][j] == '1') {
-                    queue.addLast(new int[]{j, i});
+                    DFSMarking(grid, i, j);
+                    ++count;
                 }
-            }
         }
+        return count;
+    }
 
-        while (!queue.isEmpty()) {
-            int[] currPos = queue.poll();
-            if (grid[currPos[1]][currPos[0]] == '0') {
-                continue;
-            }
-            result++;
-
-            Deque<int[]> stack = new ArrayDeque<>();
-            stack.add(currPos);
-
-            while(!stack.isEmpty()) {
-                int[] curr = stack.pollLast();
-                
-                if (grid[curr[1]][curr[0]] == '0') {
-                    continue;
-                }
-
-                grid[curr[1]][curr[0]] = '0';
-
-                for (int i = 0 ; i < 4 ; i++) {
-                    int nextX = dx[i] + curr[0];
-                    int nextY = dy[i] + curr[1];
-
-                    if (nextX >= grid[0].length || nextX < 0 || nextY >= grid.length || nextY < 0) {
-                        continue;
-                    }
-
-                    stack.addLast(new int[]{nextX, nextY});
-                }
-            
-            }
-        }
-
-        return result;
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1')
+            return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
     }
 }
