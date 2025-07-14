d68c05ebc79f7a0c28f1906f9cf24564d8e5b645
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-07-14 23:48:16 +0900

    Time: 1 ms (100%), Space: 42.5 MB (70.5%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -1,22 +1,19 @@
 class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        
-        int[] xs = new int[]{ax1, ax2, bx1, bx2};
-        int[] ys = new int[]{ay1, ay2, by1, by2};
 
-        Arrays.sort(xs);
-        Arrays.sort(ys);
-        int dup = 0;
+        int areaOfSqrA = (ax2 - ax1) * (ay2 - ay1);
+        int areaOfSqrB = (bx2 - bx1) * (by2 - by1);
 
-        if ((((bx1 <= ax1 && bx1 <= ax2) && (bx2 <= ax1 && bx2 <= ax2)) || ((bx1 >= ax1 && bx1 >= ax2) && (bx2 >= ax1 && bx2 >= ax2))) || (((by1 <= ay1 && by1 <= ay2) && (by2 <= ay1 && by2 <= ay2)) || ((by1 >= ay1 && by1 >= ay2) && (by2 >= ay1 && by2 >= ay2)))) {
-            
-            dup = 0; 
-        } else {
-            
-            dup = (xs[2] - xs[1]) * (ys[2] - ys[1]);
-        }
+        int left = Math.max(ax1, bx1);
+        int right = Math.min(ax2, bx2);
+        int bottom = Math.max(by1, ay1);
+        int top = Math.min(by2, ay2);
 
+        //If overlap
+        int overlap = 0;
+        if (right > left && top > bottom)
+            overlap = (right - left) * (top - bottom);
 
-        return (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1) - dup;
+        return areaOfSqrA + areaOfSqrB - overlap;
     }
 }
