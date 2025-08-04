5a9e75b1b371f4ad6d56ce982f15870caca08b4e
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-08-05 00:14:43 +0900

    Time: 7 ms (91.54%), Space: 60.5 MB (36.04%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,47 +1,24 @@
 class Solution {
-
-    Deque<int[]> queue = new ArrayDeque<>();
-
     public int maximalSquare(char[][] matrix) {
+        
+        if(matrix == null || matrix.length == 0) return 0;
 
-        int maxX = matrix[0].length;
-        int maxY = matrix.length;
-        int size = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        for(int currY = 0 ; currY < maxY ; currY++) {
-            for(int currX = 0 ; currX < maxX ; currX++) {
-                if(matrix[currY][currX] == '1') {
-                    queue.add(new int[]{currY, currX});
+        int[][] dp = new int[m + 1][n + 1];
+        int maxLen = 0;
+
+        for(int i = 1 ; i <= m ; i++) {
+            for(int j = 1 ; j <= n ; j++) {
+                if(matrix[i- 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+
+                    maxLen = Math.max(maxLen, dp[i][j]);
                 }
             }
         }
 
-        while(!queue.isEmpty()) {
-            int[] curr = queue.peekFirst();
-            if(isSquare(matrix, curr[1], curr[0], size, maxX, maxY)) {
-                size++;
-            } else {
-                queue.removeFirst();
-            }
-        }
-
-        return (int) Math.pow(Math.max(size - 1, 0), 2);
-    }
-
-    private boolean isSquare(char[][] matrix, int x, int y, int size, int maxX, int maxY) {
-        if(x + size - 1 >= maxX || y + size - 1 >= maxY) {
-            
-            return false;
-        }
-
-        for(int currY = y ; currY < y + size ; currY++) {
-            for(int currX = x ; currX < x + size ; currX++) {
-                if(matrix[currY][currX] == '0') {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+        return maxLen * maxLen;
     }
 }
