1fce247ae01bdc3c0e18a8513de37be4ab9d43b9
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-08-08 21:50:53 +0900

    Time: 21 ms (13.51%), Space: 56.1 MB (7.25%) - LeetHub

--- 0881-loud-and-rich/0881-loud-and-rich.java
+++ 0881-loud-and-rich/0881-loud-and-rich.java
@@ -1,18 +1,14 @@
 class Solution {
 
-    Map<Integer, Set<Integer>> richerHis = new HashMap<>();
+    Map<Integer, Set<Integer>> richerMap = new HashMap<>();
+    int[] answer;
+    int[] quiet;
 
     public int[] loudAndRich(int[][] richer, int[] quiet) {
 
-        int[] answer = new int[quiet.length];
-        Map<Integer, Set<Integer>> richerMap = new HashMap<>();
-        
-
-        for (int i = 0; i < quiet.length; i++) {
-            Set<Integer> richerSet = richerMap.getOrDefault(i, new HashSet<>());
-            richerSet.add(i);
-            richerMap.put(i, richerSet);
-        }
+        this.quiet = quiet;
+        answer = new int[quiet.length];
+        Arrays.fill(answer, -1);
 
         for (int[] r : richer) {
             Set<Integer> richerSet = richerMap.getOrDefault(r[1], new HashSet<>());
@@ -21,43 +17,25 @@ class Solution {
         }
 
         for (int i = 0; i < quiet.length; i++) {
-            Set<Integer> richerSet = findRicher(i, richerMap);
-            int quietVal = Integer.MAX_VALUE;
-            int quietIdx = 0;
-
-            for (int j : richerSet) {
-                if (quiet[j] < quietVal) {
-                    quietIdx = j;
-                    quietVal = quiet[j];
-                }
-            }
-            answer[i] = quietIdx;
+            dfs(i);
         }
 
         return answer;
     }
 
-    private Set<Integer> findRicher(int x, Map<Integer, Set<Integer>> richerMap) {
+    private int dfs(int x) {
 
-        if(richerHis.get(x) != null) {
-            return richerHis.get(x);
-        }
-
-        Set<Integer> richerSet = richerMap.getOrDefault(x, new HashSet<>());
-
-        if (richerSet.isEmpty()) {
-            return richerSet;
-        } else {
-            Set<Integer> newSet = new HashSet<>();
-            newSet.addAll(richerSet);
-            for (int i : richerSet) {
-                if (i == x)
-                    continue;
-                newSet.addAll(findRicher(i, richerMap));
+        if (answer[x] == -1) {
+            answer[x] = x;
+            for(int y : richerMap.getOrDefault(x, new HashSet<>())) {
+                int quietest = dfs(y);
+                if(quiet[answer[x]] > quiet[quietest]) {
+                    answer[x] = quietest;
+                }
             }
-
-            richerHis.put(x, newSet);
-            return newSet;
+            
         }
+
+        return answer[x];
     }
 }
