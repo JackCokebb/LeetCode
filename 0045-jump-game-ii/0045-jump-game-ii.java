9f77f572fc9232c58a94e6a194cf5cb477640b9a
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-08-08 23:46:33 +0900

    Time: 1 ms (99.51%), Space: 45.1 MB (73.19%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,20 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
         
-        int n = nums.length;
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        dp[1] = 1;
+        int near = 0, far = 0, jumps = 0;
 
-        for(int i = 0 ; i < n - 1 ; i++) {
-            for(int j = 1 ; j <= nums[i] ; j++) {
-                if (i + j >= n) continue;
-                dp[i + j] = Math.min(dp[i] + 1, dp[i + j]);
-                //System.out.println(Arrays.toString(dp));
+        while(far < nums.length - 1) {
+            int farthest = 0;
+            for(int i = near ; i <= far ; i++) {
+                farthest = Math.max(i + nums[i], farthest);
             }
+            near = far + 1;
+            far = farthest;
+            jumps++;
         }
 
-        return dp[n - 1];
+        return jumps;
     }
 }
