21651e35ee54369c938aea8fd23fb761d83c8ca3
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-08-08 21:52:57 +0900

    Time: 5 ms (99.72%), Space: 55.1 MB (52.77%) - LeetHub

--- 0881-loud-and-rich/0881-loud-and-rich.java
+++ 0881-loud-and-rich/0881-loud-and-rich.java
@@ -1,45 +1,36 @@
 class Solution {
-
-    Map<Integer, Set<Integer>> richerMap = new HashMap<>();
+    ArrayList<Integer>[] graph;
     int[] answer;
     int[] quiet;
 
     public int[] loudAndRich(int[][] richer, int[] quiet) {
-
+        int N = quiet.length;
+        graph = new ArrayList[N];
+        answer = new int[N];
         this.quiet = quiet;
-        answer = new int[quiet.length];
+
+        for (int node = 0; node < N; ++node)
+            graph[node] = new ArrayList<Integer>();
+
+        for (int[] edge: richer)
+            graph[edge[1]].add(edge[0]);
+
         Arrays.fill(answer, -1);
 
-        for (int i = 0; i < quiet.length; i++) {
-            richerMap.put(i, new HashSet<>());
-        }
-
-        for (int[] r : richer) {
-            Set<Integer> richerSet = richerMap.get(r[1]);
-            richerSet.add(r[0]);
-            richerMap.put(r[1], richerSet);
-        }
-
-        for (int i = 0; i < quiet.length; i++) {
-            dfs(i);
-        }
-
+        for (int node = 0; node < N; ++node)
+            dfs(node);
         return answer;
     }
 
-    private int dfs(int x) {
-
-        if (answer[x] == -1) {
-            answer[x] = x;
-            for(int y : richerMap.get(x)) {
-                int quietest = dfs(y);
-                if(quiet[answer[x]] > quiet[quietest]) {
-                    answer[x] = quietest;
-                }
+    public int dfs(int node) {
+        if (answer[node] == -1) {
+            answer[node] = node;
+            for (int child: graph[node]) {
+                int cand = dfs(child);
+                if (quiet[cand] < quiet[answer[node]])
+                    answer[node] = cand;
             }
-            
         }
-
-        return answer[x];
+        return answer[node];
     }
 }
