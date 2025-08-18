be3bf5d6cb3c75b961ae287b9f51bda472a14c66
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-08-19 00:21:45 +0900

    Time: 5 ms (45.82%), Space: 43.9 MB (13.66%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,70 +1,24 @@
 class Solution {
+    List<String> ans = new ArrayList<>();
+    String str;
     public List<String> restoreIpAddresses(String s) {
+        str = s;
+        magical("", 0, 0);
+        return ans;
+    } 
 
-        List<String> answer = new ArrayList<>();
-
-        if (s.length() < 4 || s.length() > 12) {
-
-            return answer;
+    void magical( String path, int index, int dots) {
+        if (dots > 4) return;
+        if (dots == 4 && index >= str.length()) {
+            ans.add(path.substring(0,path.length()-1));
+            return;
         }
-
-        return divideAsIp(s, 4);
-    }
-
-    public List<String> divideAsIp(String s, int size) {
-
-        List<String> answer = new ArrayList<>();
-
-        if (size * 3 < s.length()) {
-            return answer;
-        }
-
-        if (size == 1 && validateSingle(s)) {
-            answer.add(s);
-            return answer;
-        }
-
-        for (int i = 1; i <= 3; i++) {
-            if (s.length() - 1 < i) {
-                break;
-            }
-            String sub = s.substring(i, s.length());
-            String head = s.substring(0, i);
-
-            if(!validateSingle(head)) {
-                continue;
-            }
-
-            //System.out.println("sub: " + sub + ", head: " + head);
-            List<String> divResult = divideAsIp(sub, size - 1);
-
-            if (!divResult.isEmpty()) {
-                for (String r : divResult) {
-                    answer.add(head + "." + r);
-                }
+        for (int length = 1; length <= 3 && index + length <= str.length(); length++) {
+            String num = str.substring(index, index + length);
+            if (num.charAt(0) == '0' && length != 1) break;
+            else if (Integer.parseInt(num) <= 255) {
+                magical( path + str.substring(index, index + length) + ".", index + length,dots + 1);
             }
         }
-
-        return answer;
-    }
-
-    public boolean validateSingle(String s) {
-        int len = s.length();
-
-        if (len <= 0 || len > 3) {
-            return false;
-        }
-
-        if ((len >= 2 && s.charAt(0) == '0')) {
-            return false;
-        }
-
-        int val = Integer.valueOf(s);
-
-        if (val < 0 || val > 255) {
-            return false;
-        }
-
-        return true;
     }
 }
