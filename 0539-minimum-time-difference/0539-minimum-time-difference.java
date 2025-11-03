8dbf5c4aeafb094c295fc2736f968bcd8e31667d
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2025-11-03 22:40:01 +0900

    Time: 11 ms (34.86%), Space: 47.9 MB (5.18%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,35 +1,27 @@
-import java.time.*;
-
 class Solution {
+
     public int findMinDifference(List<String> timePoints) {
-        
-        Set<String> times = new HashSet<>(timePoints);
-        
-        int min = Integer.MAX_VALUE;
-        int setSize = times.size();
-
-        if(timePoints.size() != setSize) {
-            return 0;
+        // convert input to minutes
+        int[] minutes = new int[timePoints.size()];
+        for (int i = 0; i < timePoints.size(); i++) {
+            String time = timePoints.get(i);
+            int h = Integer.parseInt(time.substring(0, 2));
+            int m = Integer.parseInt(time.substring(3));
+            minutes[i] = h * 60 + m;
         }
 
-        timePoints = new ArrayList<>(times);
+        // sort times in ascending order
+        Arrays.sort(minutes);
 
-        for(int i = 0 ; i < setSize ; i++) {
-            for(int j = i + 1 ; j < setSize ; j++) {
-                min = Math.min(timeDiff(timePoints.get(i), timePoints.get(j)), min);
-            }
+        // find minimum difference across adjacent elements
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < minutes.length - 1; i++) {
+            ans = Math.min(ans, minutes[i + 1] - minutes[i]);
         }
 
-        return min;
-    }
-
-    private int timeDiff(String x, String y) {
-
-        LocalTime time1 = LocalTime.parse(x);
-        LocalTime time2 = LocalTime.parse(y);
-
-        int diff = Math.abs((int) Duration.between(time1, time2).toMinutes());
-
-        return Math.min(diff, 1440 - diff);
+        // consider difference between last and first element
+        return Math.min(
+                ans,
+                24 * 60 - minutes[minutes.length - 1] + minutes[0]);
     }
 }
