c05925e6d932a62f6525ad0acbfd693dc771cd03
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2026-05-09 18:15:04 +0900

    Time: 0 ms (100%), Space: 42.5 MB (94.25%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,62 +1,37 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        if (k > s.length()) {
-            return 0;
-        }
-
-        char[] chars = s.toCharArray();
-        int len = chars.length;
-        int targetLen = len;
-        int left = 0;
-        int right = len - 1;
-        int[] freqs = new int[26];
-
-        for (int i = 0; i < len; i++) {
-            freqs[chars[i] - 'a']++;
-        }
-
-        if (isFulfilled(freqs, k)) {
-            return targetLen;
-        }
-
-        while (targetLen >= k) {
-            targetLen--;
-            left = len - targetLen;
-            right = len - 1;
-            int[] currArr = Arrays.copyOf(freqs, freqs.length);
-            
-            for (int i = 0; i < left; i++) {
-                currArr[chars[i] - 'a']--;
-            }
-            //System.out.println("L: " + left + ", R: " + right);
-            if (isFulfilled(currArr, k)) {
-                return targetLen;
-            }
-
-            while (left > 0) {
-                left--; right--;
-                //System.out.println("L: " + left + ", R: " + right);
-                currArr[chars[left] - 'a']++;
-                if (right + 1 < len) {
-                    currArr[chars[right + 1] - 'a']--;
-                }
-                if (isFulfilled(currArr, k)) {
-                    return targetLen;
-                }
-            }
-        }
-
-        return 0;
+        if (k < 2) return s.length();
+        
+        return divideAndConquer(s, 0, s.length(), k);
     }
 
-    private boolean isFulfilled(int[] freqs, int k) {
-        for (int i = 0 ; i < 26; i++) {
-            if (freqs[i] < k && freqs[i] != 0) {
-                //System.out.println("" + (char)(i + 'a') + ": " + freqs[i]);
-                return false;
+    private int divideAndConquer(String s, int start, int end, int k) {
+        if (start >= end) return 0;
+        int[] freq = new int[26];
+        boolean valid = true;
+
+        for (int i = start; i < end; i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        for (int i = start; i < end; i++) {
+            if (freq[s.charAt(i) - 'a'] < k && freq[s.charAt(i) - 'a'] > 0) {
+                valid = false;
+            }
+        }
+        if (valid) return end - start;
+
+        int result = 0;
+        int currStart = start;
+
+        for (int i = start; i < end; i++) {
+            if (freq[s.charAt(i) - 'a'] < k && freq[s.charAt(i) - 'a'] > 0) {
+                result = Math.max(divideAndConquer(s, currStart, i, k), result);
+                currStart = i + 1;
             }
         }
 
-        return true;
+        result = Math.max(divideAndConquer(s, currStart, end, k), result);
+
+        return result;
     }
 }
