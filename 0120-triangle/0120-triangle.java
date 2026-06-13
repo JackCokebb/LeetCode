dea3d9a6bf7478f51cf7fded6f6d196a099026cf
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2026-06-13 14:02:16 +0900

    Time: 3 ms (83.56%), Space: 45.2 MB (83.64%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,24 +1,18 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int maxDepth = triangle.size();
-        List<List<Integer>> dp = new ArrayList<>();
+        int[] dp = new int[maxDepth];
 
-        for (int i = 0; i < maxDepth; i++) {
-            dp.add(new ArrayList<>());
-        }
-
-        for (int i : triangle.get(maxDepth - 1)) {
-            dp.get(maxDepth - 1).add(i);
+        for (int j = 0; j < maxDepth; j++) {
+            dp[j] = triangle.get(maxDepth - 1).get(j);
         }
 
         for (int i =  maxDepth - 2; i >= 0; i--) {
-            List<Integer> curr = triangle.get(i);
-            List<Integer> prev = dp.get(i + 1);
-            for (int j = 0; j < curr.size(); j++) {
-                dp.get(i).add(Math.min(curr.get(j) + prev.get(j), curr.get(j) + prev.get(j + 1)));
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
             }
         }
 
-        return dp.get(0).get(0);
+        return dp[0];
     }
 }
