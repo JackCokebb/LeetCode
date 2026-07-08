fe4c8a4f93ba55f6765db629598265a42e65a804
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2026-07-08 23:24:19 +0900

    Time: 16 ms (33.95%), Space: 70.2 MB (5.07%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,64 +8,16 @@
  * }
  */
 class Solution {
-    ArrayDeque<TreeNode> ppq;
-    ArrayDeque<TreeNode> qpq;
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        find(root, p, q, new ArrayDeque<>());
-        //printPriorityQueue(ppq);
-        //printPriorityQueue(qpq);
+        if (root == null) return null;
 
-        while((!ppq.isEmpty() && !qpq.isEmpty()) && ppq.peekLast().val != qpq.peekLast().val) {
-            if (ppq.size() < qpq.size()) {
-                TreeNode polled = qpq.pollLast();
-                //System.out.println("!" + polled.val + " was polled from qpq");
-            } else {
-                TreeNode polled = ppq.pollLast();
-                //System.out.println("!" + polled.val + " was polled from ppq");
-            }
-        }
+        if (p.val == root.val || q.val == root.val) return root;
 
-        return ppq.peekLast();
-    }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-    private void find(TreeNode root, TreeNode p, TreeNode q, ArrayDeque<TreeNode> visited) {
-        if (root == null) return;
+        if (left != null && right != null) return root;
 
-        visited.addLast(root);
-        //System.out.println("pval: " + p.val + ", q.val: " + q.val + ", rootval: " + root.val);
-
-        if (p.val == root.val) {
-            ppq = new ArrayDeque<>();
-            ppq.addAll(visited);
-            //System.out.println("ppq added");
-        }
-
-        if (q.val == root.val) {
-            qpq = new ArrayDeque<>();
-            qpq.addAll(visited);
-            //System.out.println("qpq added");
-        }
-
-        if (ppq != null && qpq != null) return;
-        
-
-        find(root.left, p, q, visited);
-        find(root.right, p, q, visited);
-        visited.pollLast();
-    }
-
-    public static void printPriorityQueue(ArrayDeque<TreeNode> queue) {
-        ArrayDeque<TreeNode> copy = new ArrayDeque<>(queue);
-
-        System.out.print("[");
-        while (!copy.isEmpty()) {
-            System.out.print(copy.pollLast().val);
-
-            if (!copy.isEmpty()) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]");
+        return left != null ? left : right;
     }
 }
