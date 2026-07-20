a9d51390e7ebff2eec1f25a5917892b4d00409ff
JackCokebb <81720768+JackCokebb@users.noreply.github.com>
2026-07-20 23:00:16 +0900

    Time: 3 ms (99.88%), Space: 46.2 MB (90.58%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,76 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
+        int[] values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] symbol={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         StringBuilder sb = new StringBuilder();
-
-        if (num / 1000 != 0) {
-            for (int i = 0; i < num / 1000; i++) {
-                sb.append("M");
-            }
-            num = num % 1000;
-        }
-
-        if (num / 100 != 0) {
-            if (num / 100 == 9) {
-                sb.append("CM");
-                num -= 900;
-            } else if (num / 100 == 4) {
-                sb.append("CD");
-                num -= 400;
-            } 
-            
-            if (num / 100 >= 5) {
-                sb.append("D");
-                num -= 500;
-            }
-
-            for (int i = 0; i < num / 100; i++) {
-                sb.append("C");
-            }
-
-            num = num % 100;
-        }
-
-        if (num / 10 != 0) {
-            if (num / 10 == 9) {
-                sb.append("XC");
-                num -= 90;
-            } else if (num / 10 == 4) {
-                sb.append("XL");
-                num -= 40;
-            } 
-            
-            
-            if (num / 10 >= 5) {
-                sb.append("L");
-                num -= 50; 
-            }
-
-            for (int i = 0; i < num / 10; i++) {
-                sb.append("X");
-            }  
-
-            num = num % 10;
-        }
-
-        if (num != 0) {
-            if (num == 9) {
-                sb.append("IX");
-                num -= 9;
-            } else if (num == 4) {
-                sb.append("IV");
-                num -= 4;
-            } 
-            
-            if (num >= 5) {
-                sb.append("V");
-                num -= 5;
-            }
-
-            for (int i = 0; i < num; i++) {
-                sb.append("I");
+        
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                sb.append(symbol[i]);
+                num -= values[i];
             }
         }
-
+        
         return sb.toString();
     }
 }
